ad90de8d39589aaccbd9749fdeca35c9901949f3
Jocelino Neto <netojocelino@gmail.com>
2021-03-01 21:31:33 -0300

    test: User View render 'create.json' Test Case

---
+++ test/rocketpay_web/views/users_view_test.exs
@@ -0,0 +1,38 @@
+defmodule RocketpayWeb.UsersViewTest do
+  use  RocketpayWeb.ConnCase
+
+  import Phoenix.View
+
+  alias Rocketpay.{Account, User}
+
+  test "renders create.json" do
+    params = %{
+      name: "Jocelino",
+      password: "123456",
+      nickname: "jocelino",
+      email: "netojocelino@github.com",
+      age: 24
+    }
+
+    {:ok, %User{id: user_id, account: %Account{id: account_id}} = user} =
+      Rocketpay.create_user(params)
+
+    response = render(RocketpayWeb.UsersView, "create.json", user: user)
+
+    expected_response =  %{
+        message: "User created",
+        user: %{
+          account: %{
+            balance: Decimal.new("0.00"),
+            id: account_id
+          },
+          id: user_id,
+          name: "Jocelino",
+          nickname: "jocelino"
+        }
+      }
+
+    assert expected_response == response
+  end
+
+end
